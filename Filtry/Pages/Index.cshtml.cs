23e4b49b0ac83f8a671e4ad4d0dbdc9302a1aefc
Julia <julia1251@wp.pl>
2021-05-17 23:49:23 +0200

    gotowe

---
+++ Filtry/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filtry.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next) 
+        { 
+            ViewData["ip"] = context.HttpContext.Connection.RemoteIpAddress; 
+            await next(); 
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
